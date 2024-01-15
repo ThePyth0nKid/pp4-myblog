6e0480cc0018c6e1d4c366f345acb40999386f1f
ThePyth0nKid <>
2024-01-15 10:32:48 +0000

    Connect the enviroment variable DATABASE_URL

--- my_project/settings.py
+++ my_project/settings.py
@@ -81,13 +81,16 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
