5859c17f9d0572159606f13d65fe5d5a080b85b5
ThePyth0nKid <>
2024-01-18 11:32:52 +0000

    Add index.html file with basic content to blog/templates/blog

--- my_project/settings.py
+++ my_project/settings.py
@@ -18,6 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# Set the path to the templates directory (within the base directory)
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
@@ -64,7 +65,7 @@ ROOT_URLCONF = 'my_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
