509b9b3f82682fbf870110d0337d0b638f791fa8
ThePyth0nKid <>
2024-01-22 10:40:04 +0000

    Add forms.py and urls.py to contact_me

--- contact_me/admin.py
+++ contact_me/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Contact
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'email', 'message', 'created_at')
+    summernote_fields = ('message',)
--- contact_me/views.py
+++ contact_me/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import ContactForm
 
 
-# Create your views here.
-def contact_me(request):
-    return HttpResponse("This is the contact page")
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
