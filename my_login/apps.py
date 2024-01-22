cc7e8c3a3ac6777c8b113d6fef39561762cf80e6
ThePyth0nKid <>
2024-01-22 09:05:24 +0000

    Delete my_login and register

--- my_login/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyLoginConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'my_login'
--- my_project/settings.py
+++ my_project/settings.py
@@ -49,8 +49,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'django_summernote',
     'blog',
-    'register',
-    'my_login',
     'contact_me',
 ]
 
