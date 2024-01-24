42d04ff83d84b9c6134caadf52cd93d850a06d7a
ThePyth0nKid <>
2024-01-24 08:32:19 +0000

    Added message display functionality to base.html

--- contact_me/views.py
+++ contact_me/views.py
@@ -1,17 +1,1 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from .forms import ContactForm
-
-
-def contact_view(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-            # Add a success message
-            messages.success(request, 'Thank you! Your message has been sent.')
-            return redirect('home')
-    else:
-        form = ContactForm()
-
-    return render(request, 'contact_me/contact.html', {'form': form})
+git add 
