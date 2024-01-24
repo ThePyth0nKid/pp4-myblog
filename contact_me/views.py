72886262394c82801ba24ab24c8c6d9357d55cdd
ThePyth0nKid <>
2024-01-24 08:19:09 +0000

    Add redirect and messages.success to contact_me/views.py

--- contact_me/views.py
+++ contact_me/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import ContactForm
 
 
@@ -7,6 +8,10 @@ def contact_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            # Add a success message
+            messages.success(request, 'Thank you! Your message has been sent.')
+            return redirect('home')
     else:
         form = ContactForm()
+
     return render(request, 'contact_me/contact.html', {'form': form})
