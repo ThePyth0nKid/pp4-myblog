dd334c9d964d6ff5e295683dfdbb8ae69aea45eb
ThePyth0nKid <>
2024-01-26 08:36:40 +0000

    Add comment display and count to blog post_detail.html template

--- blog/views.py
+++ blog/views.py
@@ -25,11 +25,25 @@ def post_detail(request, slug):
     :template:`blog/post_detail.html`
     """
 
+    # Filter and retrieve all Post objects where status equals 1
     queryset = Post.objects.filter(status=1)
+
+    # Get the specific post by its slug, or raise a 404 error if not found
     post = get_object_or_404(queryset, slug=slug)
 
+    # Retrieve all comments for the post, ordering them from newest to oldest
+    comments = post.comments.all().order_by("-created_on")
+
+    # Count the number of approved comments for the post
+    comment_count = post.comments.filter(approved=True).count()
+
+    # Render the post detail page with post data and comments
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+        },
     )
