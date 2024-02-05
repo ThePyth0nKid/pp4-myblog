b1477aa03500d86e90741179a68a6fd3cc9bae25
ThePyth0nKid <>
2024-02-05 17:48:29 +0000

    Add message when user is redirected due to lack of permissions

--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -1,25 +1,38 @@
 from django.shortcuts import render, redirect
 from contact_me.models import Contact
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from .forms import PostForm
 from django.contrib import messages
 
 
-
+@login_required
 def admin_messages(request):
+    """
+    Displays a list of messages from the 'Contact' model for superusers.
+
+    - If the user is a superuser, it retrieves all 'Contact' objects and displays them in the 'admin_dashboard.html' template.
+    - If the user is not a superuser, it redirects them to the 'home' page.
+
+    Args:
+        request: HttpRequest object containing metadata about the request.
+
+    Returns:
+        HttpResponse object rendering the 'admin_dashboard.html' template with the list of 'Contact' objects.
+    """
     if request.user.is_authenticated and request.user.is_superuser:
-        # Angenommen, es gibt ein Feld 'created_at'
+        # Assuming there is a 'created_at' field
         contacts = Contact.objects.all()
         return render(request, 'admin_dashboard/admin_dashboard.html', {'contacts': contacts})
     else:
-
+        # If the user is not a superuser, redirect them to the 'home' page
         return redirect('home')
 
 
 @login_required
 def create_post(request):
     """
-    Creates a new blog post. This view is only accessible to authenticated users.
+    Creates a new blog post. This view is only accessible to authenticated superusers.
 
     - If the request method is POST, it attempts to create a new post using the submitted form data.
     - Validates the form data, and if valid, sets the post's author to the currently logged-in user and saves the post.
@@ -32,20 +45,26 @@ def create_post(request):
     Returns:
         HttpResponse object that renders the 'create_post.html' template with the post creation form.
     """
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            new_post = form.save(commit=False)
-            # Set the post's author to the currently logged-in user
-            new_post.author = request.user
-            new_post.save()
-            # Display a success message to the user
-            messages.success(
-                request, 'The post has been successfully created!')
-            # Redirect to the home page after successful post creation
-            return redirect('home')
-    else:
-        form = PostForm()
+    if request.user.is_authenticated and request.user.is_superuser:
+        if request.method == 'POST':
+            form = PostForm(request.POST, request.FILES)
+            if form.is_valid():
+                new_post = form.save(commit=False)
+                # Set the post's author to the currently logged-in user
+                new_post.author = request.user
+                new_post.save()
+                # Display a success message to the user
+                messages.success(
+                    request, 'The post has been successfully created!')
+                # Redirect to the home page after successful post creation
+                return redirect('home')
+        else:
+            form = PostForm()
 
-    # Render the post creation form for GET requests or if form validation fails
-    return render(request, 'admin_dashboard/create_post.html', {'form': form})
+        # Render the post creation form for GET requests or if form validation fails
+        return render(request, 'admin_dashboard/create_post.html', {'form': form})
+    else:
+        # If the user is not authenticated or not a superuser, redirect them to the 'home' page
+        messages.warning(
+            request, 'You do not have permission to create a post.')
+        return redirect('home')
