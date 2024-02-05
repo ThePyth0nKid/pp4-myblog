4b4b1cff97155af321751a444371b2ffd5d5cc77
ThePyth0nKid <>
2024-02-05 18:05:21 +0000

    Add navbar link for post creation and fix NoReverseMatch error by updating URL names and template references

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -4,5 +4,5 @@ from .views import admin_messages, create_post
 
 urlpatterns = [
     path('', admin_messages, name='admin_dashboard'),
-    path('create/', create_post, name='create-post')
+    path('create/', create_post, name='create_post')
 ]
