3d78f9b6951eefa341f5009ad30843a80682d5b8
ThePyth0nKid <>
2024-02-06 20:28:18 +0000

    Collectstatic for depolyment

--- static/js/delete-post.js
+++ static/js/delete-post.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Accessing the post deletion modal using the updated ID
-    const deletePostModal = document.getElementById('deletePostModal');
-    deletePostModal.addEventListener('show.bs.modal', function (event) {
+    const deleteModal = document.getElementById('deleteModal');
+    deleteModal.addEventListener('show.bs.modal', function (event) {
         // Button that triggered the modal
         const button = event.relatedTarget;
         // Extract info from data-* attributes
         const postSlug = button.getAttribute('data-postslug');
         // Dynamically construct the delete URL based on the post's slug
         const deleteUrl = `/admin_dashboard/delete/${postSlug}/`;
-        // Find the delete confirmation button in the modal using the updated ID and set its href to the delete URL
-        const deletePostConfirmButton = deletePostModal.querySelector('#deletePostConfirm');
-        deletePostConfirmButton.href = deleteUrl;
+        // Find the delete confirmation button in the modal and set its href to the delete URL
+        const deleteConfirmButton = deleteModal.querySelector('#deleteConfirm');
+        deleteConfirmButton.href = deleteUrl;
     });
 });
+
